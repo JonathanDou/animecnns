a3d45195730992b2b993da31d09484b0167b7887
Luke McDevitt <luke_mcdevitt@brown.edu>
2021-04-19 05:17:52 -0400

    fuqq

--- code/main.py
+++ code/main.py
@@ -3,6 +3,8 @@ import numpy as np
 import random
 import cv2
 import os
+from tensorflow.keras.layers import \
+    Conv2D, MaxPool2D, Dropout, Flatten, Dense, BatchNormalization
 
 def load_images_from_folder(folder):
     
@@ -18,6 +20,7 @@ def load_images_from_folder(folder):
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             img = cv2.resize(img, (28, 28)) 
             img = img / 255
+            img = np.expand_dims(img, axis=0)
             images.append(img)
             
     return images
@@ -26,6 +29,7 @@ traingirls = load_images_from_folder("../traindata/girls")
 trainguys = load_images_from_folder("../traindata/guys")
 testgirls = load_images_from_folder("../testdata/girls")
 testguys = load_images_from_folder("../testdata/guys")
+print(np.asarray(testguys).shape)
 
 traindata = traingirls + trainguys  
 testdata = testgirls + testguys 
@@ -47,6 +51,13 @@ testdata = testdata[indices]
 testlabels = testlabels[indices]
 
 model = tf.keras.Sequential([
+    
+    Conv2D(28, kernel_size=(3, 3), activation='relu',padding='same'),
+    (MaxPool2D(pool_size=(2, 2), padding='same')),
+    Conv2D(28, kernel_size=(3, 3), activation='relu',padding='same'),
+    BatchNormalization(),
+    (MaxPool2D(pool_size=(2, 2), padding='same')),
+    Dropout(0.3),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
@@ -60,6 +71,6 @@ model.compile(optimizer='Adam',
 
 model.fit(traindata, trainlabels,
           batch_size=10,
-          epochs=20,
+          epochs=200,
           verbose=1,
           validation_data=(testdata, testlabels))
