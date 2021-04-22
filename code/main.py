181475ed306bcb1b4b135040716ce478914b9037
JonathanDou <jonathandou100@gmail.com>
2021-04-21 21:27:36 -0400

    updated'

--- code/main.py
+++ code/main.py
@@ -4,9 +4,11 @@ import random
 import cv2
 from tensorflow.keras.layers import \
     Conv2D, MaxPool2D, Dropout, Flatten, Dense, BatchNormalization
+    
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
 
-resizeshape = 28
+resizeshape = 64
 
 def load_images_from_folder(folder):
 
@@ -17,11 +19,11 @@ def load_images_from_folder(folder):
 
         if img is not None:
 
-            #convert to grayscale and preprocess
-
-            # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            #preprocess
+            
             img = cv2.resize(img, (resizeshape, resizeshape)) 
             img = img / 255
+            
             images.append(img)
 
     return images
@@ -46,32 +48,31 @@ trainab = load_images_from_folder("../traindata/AB")
 trainfate = load_images_from_folder("../traindata/Fate")
 testab = load_images_from_folder("../testdata/AB")
 testfate = load_images_from_folder("../testdata/Fate")
-# print(np.asarray(testfate).shape)
 
 traindata = trainab + trainfate + trainopm + trainzero + trainrailgun + trainerased + traindbz + trainfma + trainpsycho + trainop
-testdata = testab+ testfate + testopm + testzero + testrailgun + testerased + testdbz + testfma + testpsycho + testop
+testdata = testab + testfate + testopm + testzero + testrailgun + testerased + testdbz + testfma + testpsycho + testop
 
 traindata = np.array(traindata)
 testdata = np.array(testdata)
 
 trainlabels = np.append(np.zeros(len(trainab)), np.ones((len(trainfate))))
 testlabels = np.append(np.zeros(len(testab)), np.ones((len(testfate))))
-trainlabels = np.append(trainlabels, np.zeros(len(trainopm))*2)
-testlabels = np.append(testlabels, np.zeros(len(testopm))*2)
-trainlabels = np.append(trainlabels, np.zeros(len(trainzero))*3)
-testlabels = np.append(testlabels, np.zeros(len(testzero))*3)
-trainlabels = np.append(trainlabels, np.zeros(len(trainrailgun))*4)
-testlabels = np.append(testlabels, np.zeros(len(testrailgun))*4)
-trainlabels = np.append(trainlabels, np.zeros(len(trainerased))*5)
-testlabels = np.append(testlabels, np.zeros(len(testerased))*5)
-trainlabels = np.append(trainlabels, np.zeros(len(traindbz))*6)
-testlabels = np.append(testlabels, np.zeros(len(testdbz))*6)
-trainlabels = np.append(trainlabels, np.zeros(len(trainfma))*7)
-testlabels = np.append(testlabels, np.zeros(len(testfma))*7)
-trainlabels = np.append(trainlabels, np.zeros(len(trainpsycho))*8)
-testlabels = np.append(testlabels, np.zeros(len(testpsycho))*8)
-trainlabels = np.append(trainlabels, np.zeros(len(trainop))*9)
-testlabels = np.append(testlabels, np.zeros(len(testop))*9)
+trainlabels = np.append(trainlabels, np.ones(len(trainopm))*2)
+testlabels = np.append(testlabels, np.ones(len(testopm))*2)
+trainlabels = np.append(trainlabels, np.ones(len(trainzero))*3)
+testlabels = np.append(testlabels, np.ones(len(testzero))*3)
+trainlabels = np.append(trainlabels, np.ones(len(trainrailgun))*4)
+testlabels = np.append(testlabels, np.ones(len(testrailgun))*4)
+trainlabels = np.append(trainlabels, np.ones(len(trainerased))*5)
+testlabels = np.append(testlabels, np.ones(len(testerased))*5)
+trainlabels = np.append(trainlabels, np.ones(len(traindbz))*6)
+testlabels = np.append(testlabels, np.ones(len(testdbz))*6)
+trainlabels = np.append(trainlabels, np.ones(len(trainfma))*7)
+testlabels = np.append(testlabels, np.ones(len(testfma))*7)
+trainlabels = np.append(trainlabels, np.ones(len(trainpsycho))*8)
+testlabels = np.append(testlabels, np.ones(len(testpsycho))*8)
+trainlabels = np.append(trainlabels, np.ones(len(trainop))*9)
+testlabels = np.append(testlabels, np.ones(len(testop))*9)
 
 indices = np.arange(len(traindata))
 
@@ -82,27 +83,40 @@ indices = np.arange(len(testdata))
 
 testdata = testdata[indices]
 testlabels = testlabels[indices]
-print(testdata.shape)
-print(testlabels.shape)
-print(traindata.shape)
-print(trainlabels.shape)
+
+datagen = ImageDataGenerator(
+    rotation_range=10,
+    zoom_range=0.1,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.1,
+    horizontal_flip=True,
+    fill_mode="nearest")
+
+datagen.fit(traindata)
 
 model = tf.keras.Sequential([
-    Conv2D(10, kernel_size=(3, 3), activation='relu'),
-    (MaxPool2D(pool_size=(2, 2),padding='same')),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(1, activation='softmax')
+    Conv2D(32, 3, 1, activation='relu'),
+    MaxPool2D(2),
+    Conv2D(64, 3, 1, activation='relu'),
+    MaxPool2D(2),
+    Conv2D(128, 3, 1, activation='relu'),
+    MaxPool2D(2),
+    Flatten(),
+    Dropout(0.2),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax')
 ])
 
 
+
 model.compile(optimizer='Adam',
-              loss='categorical_crossentropy',
+              loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
+              
 
-
-model.fit(traindata, trainlabels,
-          batch_size=5,
-          epochs=20,
+model.fit(datagen.flow(traindata, trainlabels, batch_size=10),
+          steps_per_epoch=int(len(traindata) / 10),
+          epochs=50,
           verbose=1,
           validation_data=(testdata, testlabels))
