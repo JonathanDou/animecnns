62d418b82dd04eda357e55e9fdc36fc09378dca9
JonathanDou <jonathandou100@gmail.com>
2021-04-21 23:40:14 -0400

    commited

--- code/main.py
+++ code/main.py
@@ -8,7 +8,7 @@ from tensorflow.keras.layers import \
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
 
-size = 224
+size = 32
 
 def load_images_from_folder(folder):
 
@@ -97,28 +97,16 @@ datagen.fit(traindata)
 
 model = tf.keras.Sequential()
 
-model.add(Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
-model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-model.add(Flatten())
-model.add(Dense(units=128,activation="relu"))
-model.add(Dense(units=64,activation="relu"))
-model.add(Dense(units=10, activation="softmax"))
+model = tf.keras.Sequential([
+    Conv2D(6, 3, activation='relu'),
+    AveragePooling2D(),
+    Conv2D(16, 3, activation='relu'),
+    AveragePooling2D(),
+    Flatten(),
+    Dense(120, activation='relu'),
+    Dense(84, activation='relu'),
+    Dense(10, activation='softmax')
+])
 
 model.compile(optimizer='Adam',
               loss='sparse_categorical_crossentropy',
