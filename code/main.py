896591ac61f40a7cf10504560ef915a0b35cc85e
JonathanDou <jonathandou100@gmail.com>
2021-04-22 00:36:47 -0400

    committed

--- code/main.py
+++ code/main.py
@@ -99,14 +99,14 @@ datagen.fit(traindata)
 model = tf.keras.Sequential()
 
 model = tf.keras.Sequential([
+    Conv2D(16, 3, activation='relu'),
+    Conv2D(16, 3, activation='relu'),
+    MaxPool2D(2, strides=2),
+    Conv2D(32, 3, activation='relu'),
     Conv2D(32, 3, activation='relu'),
     MaxPool2D(2, strides=2),
-    Conv2D(64, 5, activation='relu'),
-    MaxPool2D(2, strides=2),
-    Conv2D(128, 7, activation='relu'),
-    MaxPool2D(2, strides=2),
     Flatten(),
-    Dense(64, activation='relu'),
+    Dense(512, activation='relu'),
     Dense(10, activation='softmax')
 ])
 
