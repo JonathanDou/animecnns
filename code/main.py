ed5029f84d41efce8fbcc5e4f1a59d11c13a5f4d
JonathanDou <jonathandou100@gmail.com>
2021-04-21 23:34:52 -0400

    commited

--- code/main.py
+++ code/main.py
@@ -116,8 +116,8 @@ model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="rel
 model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
 model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
 model.add(Flatten())
-model.add(Dense(units=4096,activation="relu"))
-model.add(Dense(units=4096,activation="relu"))
+model.add(Dense(units=128,activation="relu"))
+model.add(Dense(units=64,activation="relu"))
 model.add(Dense(units=10, activation="softmax"))
 
 model.compile(optimizer='Adam',
